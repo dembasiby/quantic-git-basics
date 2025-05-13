a31de28047d81c4a1dc20c032977b9e0f62dfb9f
Demba Siby <dembasiby@gmail.com>
2025-05-13 08:34:09 +0000

    f 2

--- app.js
+++ app.js
@@ -4,4 +4,5 @@ console.log("Here comes troubles!");
 // TODO
 // Create a function that takes a string and returns the string in reverse order
 function reverseString(str) {}
+
 // Create a function that takes a string and returns the string in uppercase
